a4881f8e0d77c4801d3b57b56afca1a439013fb1
Zen-net <allyofjustice1@gmail.com>
2020-07-02 19:58:28 +0700

    Updates

---
+++ precache-manifest.531c2490d02ae1fce7b2ffa1dd8a2b27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261f442dec3d2603d5e6e33f84c21dd8",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "7d004e8c27d2fb06bf65",
+    "url": "/calculator/static/js/2.1e7a8cfc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.1e7a8cfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "755502190b480c860d1e",
+    "url": "/calculator/static/js/main.b30ea0ab.chunk.js"
+  },
+  {
+    "revision": "34efb516125c44db6c24",
+    "url": "/calculator/static/js/runtime-main.74bc041b.js"
+  }
+]);
